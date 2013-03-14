2030cf02c18b21733063d0131863ef0c4807a475
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-03-14 17:22:38 +1000

    dupcy: added stubs for globals

--- dupcy/dupcy.py
+++ dupcy/dupcy.py
@@ -14,3 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Dupcy.  If not, see <http://www.gnu.org/licenses/>.
+
+# XXX load configuration into these globals
+links = Links()
+groups = Groups()
