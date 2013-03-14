0c7ec13c3ee32faf685eee2812ec7a4e16d61812
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-03-14 13:25:19 +1000

    Added preliminary source files and created package structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Dupcy',
+	version='1.0',
+	description='An advanced wrapper around the duplicity backup software',
+	author='Liam Edwards-Playne',
+	author_email='liamzebedee@yahoo.com.au',
+	url='https://github.com/liamzebedee/dupcy',
+	packages=['dupcy'],
+	license='GPLv3',
+)
+
