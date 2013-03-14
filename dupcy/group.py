912b860835a86061635c7f6733a83d3407c279bd
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-03-14 14:06:45 +1000

    Removed source/target specific groups in favour of generic group

---
+++ dupcy/group.py
@@ -0,0 +1,31 @@
+# Copyright 2013 Liam Edwards-Playne.
+#
+# This file is part of Dupcy.
+#
+# Dupcy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Dupcy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Dupcy.  If not, see <http://www.gnu.org/licenses/>.
+
+from urllib.parse import urlparse
+
+class Group(object):
+	def __init__(self, name):
+		self.name = name
+		self.sources = []
+		pass
+	
+	def add(self, url):
+		if url not in self.sources:
+			self.sources.append(urlparse(url))
+	
+	def remove(self, url):
+		self.sources.remove(url)
